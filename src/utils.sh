246343ae0897c03a406dd8ca715338a575c277d2
wmemcpy <celeste.francier@protonmail.com>
2023-09-23 15:59:55 +0200

    fix read_user

--- src/utils.sh
+++ src/utils.sh
@@ -1,9 +1,9 @@
 
 function read_user() {
     read -p "${1} (Y/n) " answer
-    if [[ -z "${answer}" ]] || [[ "${answer}" == "Y" ]] || [[ "${answer}" == "y" ]]; then
-        return 1
-    else
+    if [ "$answer" = "Y" ] || [ "$answer" = "y" ] || [ "$answer" = "" ]; then
         return 0
+    else
+        return 1
     fi
 }
