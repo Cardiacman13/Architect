cb297ace2e343ea28a7e8447e2b00f53d8ca23dc
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-09-24 23:07:07 +0200

    Update graphic_card.sh

--- src/setup/graphic_card.sh
+++ src/setup/graphic_card.sh
@@ -5,22 +5,23 @@ source "$BASE_DIR/src/drivers/amd.sh"
 source "$BASE_DIR/src/drivers/intel.sh"
 
 function install_video_drivers() {
-    echo "Détection de la carte graphique..."
-    while IFS= read -r video_driver; do
-        case "${video_driver}" in
-            "NVIDIA")
-                nvidia_drivers
-                ;;
-            "AMD")
-                amd_drivers
-                ;;
-            "Intel")
-                intel_drivers
-                ;;
-            *)
-                echo "|- Pas de carte graphique détectée."
-                ;;
-        esac
-    done < <(lspci -vnn | grep VGA | grep -oE 'NVIDIA|AMD|Intel')
+    echo "Quel est votre GPU ? (INTEL/AMD/NVIDIA)"
+    read -p user_gpu
+
+    case "${user_gpu}" in
+        "NVIDIA")
+            nvidia_drivers
+            ;;
+        "AMD")
+            amd_drivers
+            ;;
+        "Intel")
+            intel_drivers
+            ;;
+        *)
+            echo "Invalid GPU choice: ${user_gpu}"
+            ;;
+    esac
+
     echo "--------------------------------------------------"
 }
