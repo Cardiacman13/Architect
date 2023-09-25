f4152b9db4827e2a348359259e21cbaa96b87796
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-09-25 16:12:54 +0200

    Update intel.sh

--- src/drivers/intel.sh
+++ src/drivers/intel.sh
@@ -1,5 +1,5 @@
 
 function intel_drivers() {
-    echo "|- Carte graphique Intel détectée."
+    echo "|- Installation carte graphique Intel."
     yay -S --needed --noconfirm mesa lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader >> /dev/null 2>&1
 }
