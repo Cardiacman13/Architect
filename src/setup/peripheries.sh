70d74432da48fc96a86cda74de8dd01fbfa16759
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-09-25 15:12:29 +0200

    Update peripheries.sh

--- src/setup/peripheries.sh
+++ src/setup/peripheries.sh
@@ -15,7 +15,7 @@ function gamepad() {
 
 function printer() {
     if read_user "Voulez vous supporter les imprimantes ?"; then
-        echo "|- Installation des paquets. ${RED}(tres long)${RESET}."
+        echo -e "|- Installation des paquets. ${RED}(tres long)${RESET}."
         yay -S --needed --noconfirm ghostscript gsfonts cups cups-filters cups-pdf system-config-printer avahi foomatic-db-engine foomatic-db foomatic-db-ppds foomatic-db-nonfree foomatic-db-nonfree-ppds gutenprint foomatic-db-gutenprint-ppds >> /dev/null 2>&1
         echo "|- Activation du service d'impression et de la découverte réseau"
         sudo systemctl enable --now avahi-daemon >> /dev/null 2>&1
