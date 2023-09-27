abc13882efc3144bb112e81ac8af7a4720c2a015
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-09-27 10:39:42 +0200

    Update XFCE.sh

--- src/desktop_environments/XFCE.sh
+++ src/desktop_environments/XFCE.sh
@@ -1,5 +1,5 @@
 function config_xfce() {
-    echo -e "|- Configuration de KDE. ${RED}(long)${RESET}"
+    echo -e "|- Configuration de XFCE. ${RED}(long)${RESET}"
     yay -S --needed --noconfirm xfce4-goodies xdg-user-dirs >> /dev/null 2>&1
     xdg-user-dirs-update >> /dev/null 2>&1
 }
