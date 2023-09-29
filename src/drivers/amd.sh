fe2413f03cecacaac2e1c7751bdf8ef3cc7616d5
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-09-29 20:11:20 +0200

    Update amd.sh

--- src/drivers/amd.sh
+++ src/drivers/amd.sh
@@ -1,5 +1,5 @@
 
 function amd_drivers() {
     echo "|- Installation carte graphique AMD."
-    yay -S --needed --noconfirm mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader mesa-vulkan-layers lib32-mesa-vulkan-layers >> /dev/null 2>&1
+    yay -S --needed --noconfirm mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader vulkan-mesa-layers lib32-vulkan-mesa-layers >> /dev/null 2>&1
 }
