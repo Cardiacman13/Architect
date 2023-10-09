f041a6d3017ed85058a0b7b6425be211b10f207f
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-09 15:54:19 +0200

    Update shell.sh

--- src/setup/shell.sh
+++ src/setup/shell.sh
@@ -9,7 +9,7 @@ source "$BASE_DIR/src/utils.sh"
 #   $1: The file to add the alias to.
 function add_alias_u() {
     local file=$1
-    local alias="alias update-arch='yay && flatpak update'"
+    local alias="alias update-arch='sudo pacman -Syy && yay && flatpak update'"
     local alias_clean="alias clean-arch='yay -Sc && sudo pacman -Rns \$(pacman -Qdtq) && flatpak remove --unused'"
 
     if [[ -f "${file}" ]]; then
