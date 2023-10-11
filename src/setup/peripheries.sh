677fd38926a062b9753e8053f3b97ff5d025bff2
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-11 14:22:42 +0200

    correction dualsensectl-git

--- src/setup/peripheries.sh
+++ src/setup/peripheries.sh
@@ -14,7 +14,7 @@ function gamepad() {
     fi
     if read_user "Avez-vous une manette PS4/PS5 ?"; then
         echo "|- Installation de bluez-utils-compat, ds4drv, et dualsencectl."
-        yay -S --needed --noconfirm bluez-utils-compat ds4drv dualsencectl-git >> /dev/null 2>&1
+        yay -S --needed --noconfirm bluez-utils-compat ds4drv dualsensectl-git >> /dev/null 2>&1
     fi
     echo "--------------------------------------------------" 
 }
