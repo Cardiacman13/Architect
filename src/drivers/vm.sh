47fba2cf2704cdd3d538e7bdc6aad153c2c750c9
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-13 23:46:31 +0200

    supression swiftshader-git

--- src/drivers/vm.sh
+++ src/drivers/vm.sh
@@ -1,7 +1,7 @@
 # This function installs the necessary drivers for Virtual Machine.
 function vm_drivers() {
     echo "|- Installation pilotes pour machines virtuelles."
-    yay -S --needed --noconfirm vulkan-swrast lib32-vulkan-swrast swiftshader-git virtualbox-guest-utils >> /dev/null 2>&1
+    yay -S --needed --noconfirm vulkan-swrast lib32-vulkan-swrast virtualbox-guest-utils >> /dev/null 2>&1
     sudo systemctl enable --now vboxservice >> /dev/null 2>&1
     sudo VBoxClient-all >> /dev/null 2>&1
 }
