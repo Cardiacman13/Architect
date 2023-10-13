6af1c17fda431908b62ce394ca1d6ac9b7047ea1
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-13 23:24:28 +0200

    ajout vm

---
+++ src/drivers/vm.sh
@@ -0,0 +1,7 @@
+# This function installs the necessary drivers for Virtual Machine.
+function vm_drivers() {
+    echo "|- Installation pilotes pour machines virtuelles."
+    yay -S --needed --noconfirm vulkan-swrast lib32-vulkan-swrast swiftshader-git virtualbox-guest-utils >> /dev/null 2>&1
+    sudo systemctl enable --now vboxservice >> /dev/null 2>&1
+    sudo VBoxClient-all >> /dev/null 2>&1
+}
