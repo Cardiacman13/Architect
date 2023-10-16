a03a51cbda1689043823edcd81a166c86801e4cc
DaisZen <145337201+DaisZen@users.noreply.github.com>
2023-10-16 21:46:49 +0200

    formulation

--- src/drivers/vm.sh
+++ src/drivers/vm.sh
@@ -1,6 +1,6 @@
 # This function installs the necessary drivers for Virtual Machine.
 function vm_drivers() {
-    echo "|- Installation pilotes pour machines virtuelles."
+    echo "|- Installation des pilotes pour machines virtuelles."
     yay -S --needed --noconfirm vulkan-swrast lib32-vulkan-swrast virtualbox-guest-utils spice-vdagent qemu-guest-agent >> /dev/null 2>&1
     sudo systemctl enable --now vboxservice >> /dev/null 2>&1
     sudo VBoxClient-all >> /dev/null 2>&1
