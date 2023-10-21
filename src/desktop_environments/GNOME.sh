1eb18adddee7570d0e4dc3eb5429e0a511344360
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-21 11:47:33 +0200

    Update GNOME.sh

--- src/desktop_environments/GNOME.sh
+++ src/desktop_environments/GNOME.sh
@@ -7,7 +7,7 @@ RESET="\033[0m"
 # Set the GTK theme to adw-gtk3 using gsettings
 function config_gnome() {
     echo -e "|- Configuration de Gnome. ${RED}(long)${RESET}"
-    yay -S --needed --noconfirm adwaita-icon-theme eog evince file-roller gdm gnome-calculator gnome-console gnome-control-center gnome-disk-utility gnome-keyring gnome-nettool gnome-power-manager gnome-screenshot gnome-shell gnome-text-editor gnome-themes-extra gnome-tweaks gnome-usage gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb nautilus nautilus-sendto sushi totem xdg-user-dirs-gtk gnome-browser-connector adw-gtk3 qgnomeplatform-qt5 qgnomeplatform-qt6 >> /dev/null 2>&1
+    yay -S --needed --noconfirm adwaita-icon-theme eog evince file-roller gdm gnome-calculator gnome-console gnome-control-center gnome-disk-utility gnome-keyring gnome-nettool gnome-power-manager gnome-screenshot gnome-shell gnome-text-editor gnome-themes-extra gnome-tweaks gnome-usage gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb nautilus nautilus-sendto sushi totem xdg-user-dirs-gtk gnome-browser-connector adw-gtk3 >> /dev/null 2>&1
     gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3 >> /dev/null 2>&1
     gsettings set org.gnome.desktop.peripherals.keyboard numlock-state true >> /dev/null 2>&1    
 }
