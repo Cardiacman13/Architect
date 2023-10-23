21a39d20904d74bba63bc3811e70258cc516fe98
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-23 23:04:36 +0200

    Update graphic_card.sh

--- src/setup/graphic_card.sh
+++ src/setup/graphic_card.sh
@@ -9,7 +9,7 @@ source "$BASE_DIR/src/drivers/vm.sh"
 # It prompts the user to enter their GPU type (INTEL/AMD/NVIDIA) and installs the corresponding drivers.
 # If an invalid GPU type is entered, it displays an error message and asks again.
 function install_video_drivers() {
-    read -p ""Quelle est votre carte graphique ? (INTEL/AMD/NVIDIA/VM):" user_gpu
+    read -p "Quelle est votre carte graphique ? (INTEL/AMD/NVIDIA/VM):" user_gpu
     user_gpu=$(echo "$user_gpu" | tr '[:lower:]' '[:upper:]')  # Converts the input to uppercase
 
     while [[ -z "$user_gpu" || ! " INTEL AMD NVIDIA VM " =~ " $user_gpu " ]]; do
