1a6e7dca53f10e940b34ccb58b58112e5277a45f
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-24 19:03:00 +0200

    Update reboot_timer.sh

--- src/setup/reboot_timer.sh
+++ src/setup/reboot_timer.sh
@@ -1,1 +1,15 @@
+# Fonction de redémarrage avec un compte à rebours
+function reboot_timer() {
+    local timer=$1
 
+    echo -e "${RED}Le script est terminé, le système doit redémarrer${RESET} : Appuyez sur ${GREEN}Entrée${RESET} pour redémarrer ou ${GREEN}Ctrl+C${RESET} pour annuler."
+    read -p "" choice
+    if [[ "${choice}" == "" ]]; then
+        while [[ ${timer} -gt 0 ]]; do
+            echo -e "${RED}Redémarrage dans ${timer} secondes...${RESET}"
+            sleep 1
+            ((timer--))
+        done
+    fi
+    sudo reboot
+}
