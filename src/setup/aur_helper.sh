cfddece04d7a26a244f1cdb8f176bcceda6b9595
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-24 19:20:18 +0200

    Update aur_helper.sh

--- src/setup/aur_helper.sh
+++ src/setup/aur_helper.sh
@@ -1,20 +1,23 @@
-# Declare the global AUR helper variable
-AUR_HELPER=""   
+# Déclare la variable globale de l'AUR helper
+AUR_HELPER=""
 
-function install_aur_helper() {
+install_aur_helper() {
     local git_repo
 
-    # Always prompt the user for AUR helper choice
-    echo "Choisissez un AUR helper (yay/paru):"
-    read -p "Entrez votre choix: " AUR_HELPER 
-    AUR_HELPER=$(echo "$AUR_HELPER" | tr '[:upper:]' '[:lower:]')  # Convert input to lowercase
+    # Assurer un choix valide de l'AUR helper
+    while true; do
+        echo "Choisissez un AUR helper (yay/paru):"
+        read -p "Entrez votre choix: " AUR_HELPER 
+        AUR_HELPER=$(echo "$AUR_HELPER" | tr '[:upper:]' '[:lower:]')  # Convertir la saisie en minuscules
 
-    while [[ ! " yay paru " =~ " ${AUR_HELPER} " ]]; do
-        echo "Choix invalide."
-        read -p "Entrez yay ou paru: " AUR_HELPER
-        AUR_HELPER=$(echo "$AUR_HELPER" | tr '[:upper:]' '[:lower:]')  # Convert input to lowercase
+        if [[ "yay" == "$AUR_HELPER" || "paru" == "$AUR_HELPER" ]]; then
+            break
+        else
+            echo "Choix invalide."
+        fi
     done
 
+    # Déterminer le dépôt git en fonction du choix
     case "$AUR_HELPER" in
         yay)
             git_repo="https://aur.archlinux.org/yay-bin.git"
@@ -24,6 +27,7 @@ function install_aur_helper() {
             ;;
     esac
 
+    # Installer l'AUR helper sélectionné
     echo "Installation de $AUR_HELPER."
     sudo pacman -S --noconfirm git >> /dev/null 2>&1
     git clone $git_repo >> /dev/null 2>&1
@@ -33,6 +37,7 @@ function install_aur_helper() {
     cd .. || exit
     rm -rf "${AUR_HELPER}-bin" >> /dev/null 2>&1
 
+    # Configurations post-installation
     if [ "$AUR_HELPER" == "yay" ]; then
         yay -Y --gendb >> /dev/null 2>&1
         yay -Y --devel --save >> /dev/null 2>&1
