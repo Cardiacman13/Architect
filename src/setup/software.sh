d172aad966577efe9a57eac0b1cdce76e1c424aa
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-24 20:10:55 +0200

    Update software.sh

--- src/setup/software.sh
+++ src/setup/software.sh
@@ -1,45 +1,46 @@
-# Définit le répertoire de base
+# Définir le répertoire de base
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 
-# Inclut les fonctions utilitaires
+# Inclure les fonctions utilitaires
 source "$BASE_DIR/src/utils.sh"
 
-# Helper function to prompt the user and add software to lists
+# Fonction d'aide pour interroger l'utilisateur et ajouter des logiciels aux listes
 function prompt_and_add() {
     local prompt="$1"
     local type="$2"
-    local software="$3"
+    shift 2
+    local software=("$@")
 
     if read_user "${prompt}"; then
         if [[ "${type}" == "package" ]]; then
-            package_list+=("${software}")
+            package_list+=("${software[@]}")
         elif [[ "${type}" == "flatpak" ]]; then
-            flatpak_list+=("${software}")
+            flatpak_list+=("${software[@]}")
         fi
     fi
 }
 
-# Fonction pour installer des logiciels utiles basée sur les choix de l'utilisateur
+# Fonction pour installer des logiciels utiles en fonction de la saisie de l'utilisateur
 function install_useful_software() {
     echo "Installation des logiciels utiles."
     local package_list=()   # Liste des paquets à installer
     local flatpak_list=()   # Liste des flatpaks à installer
 
-    # Use helper function for prompting
-    prompt_and_add "|- Voulez-vous installer Discord ?", "package", "discord"
-    prompt_and_add "|- Voulez-vous installer Steam ?", "package", "steam"
-    prompt_and_add "|- Voulez-vous installer Lutris ?", "package", "lutris wine-staging"
-    prompt_and_add "|- Voulez-vous installer Heroic Games Launcher (Epic Games/GOG) ?", "package", "heroic-games-launcher-bin"
-    prompt_and_add "|- Voulez-vous installer protonup-qt ?", "package", "protonup-qt-bin"
-    prompt_and_add "|- Voulez-vous installer Spotify ?", "package", "spotify"
-    prompt_and_add "|- Voulez-vous installer OBS Studio (flatpak) ?", "flatpak", "com.obsproject.Studio"
-    prompt_and_add "|- Voulez-vous installer LibreOffice ?", "package", "libreoffice-fresh libreoffice-fresh-fr"
-    prompt_and_add "|- Voulez-vous installer Gimp ?", "package", "gimp"
-    prompt_and_add "|- Voulez-vous installer Visual Studio Code ?", "package", "visual-studio-code-bin"
-    prompt_and_add "|- Voulez-vous installer Open RGB ?", "package", "openrgb-bin"
+    # Utiliser la fonction d'aide pour l'interrogation
+    prompt_and_add "|- Voulez-vous installer Discord ?" "package" "discord"
+    prompt_and_add "|- Voulez-vous installer Steam ?" "package" "steam"
+    prompt_and_add "|- Voulez-vous installer Lutris ?" "package" "lutris" "wine-staging"
+    prompt_and_add "|- Voulez-vous installer Heroic Games Launcher (Epic Games/GOG) ?" "package" "heroic-games-launcher-bin"
+    prompt_and_add "|- Voulez-vous installer protonup-qt ?" "package" "protonup-qt-bin"
+    prompt_and_add "|- Voulez-vous installer Spotify ?" "package" "spotify"
+    prompt_and_add "|- Voulez-vous installer OBS Studio (flatpak) ?" "flatpak" "com.obsproject.Studio"
+    prompt_and_add "|- Voulez-vous installer LibreOffice ?" "package" "libreoffice-fresh" "libreoffice-fresh-fr"
+    prompt_and_add "|- Voulez-vous installer Gimp ?" "package" "gimp"
+    prompt_and_add "|- Voulez-vous installer Visual Studio Code ?" "package" "visual-studio-code-bin"
+    prompt_and_add "|- Voulez-vous installer Open RGB ?" "package" "openrgb-bin"
 
-    # Installe les paquets de la liste package_list
-    echo -e "|- Installation des paquets ${RED}(long)${RESET}"
+    # Installer les paquets de la liste package_list
+    echo -e "|- Installation des paquets ${RED}(peut être long)${RESET}"
     for package in "${package_list[@]}"; do
         echo "|- Installation de ${package}."
         if ! $AUR_HELPER -S --needed --noconfirm "${package}" >> /dev/null 2>&1; then
@@ -48,7 +49,8 @@ function install_useful_software() {
         fi
     done
 
-    # Installe les flatpaks de la liste flatpak_list
+    # Installer les flatpaks de la liste flatpak_list
+    echo "|- Installation des flatpaks."
     for flatpak in "${flatpak_list[@]}"; do
         echo "|- Installation de ${flatpak}"
         if ! flatpak install flathub -y "${flatpak}" >> /dev/null 2>&1; then
