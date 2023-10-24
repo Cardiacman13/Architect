2133264f37cdae927fcb62d56d396b5bc9569caa
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-24 20:24:01 +0200

    Update GNOME.sh

--- src/desktop_environments/GNOME.sh
+++ src/desktop_environments/GNOME.sh
@@ -1,13 +1,23 @@
-
-RED="\033[1;31m"
-RESET="\033[0m"
-
-# Function to configure GNOME desktop environment
-# Installs gnome-browser-connector and adw-gtk3 theme package using yay package manager
-# Set the GTK theme to adw-gtk3 using gsettings
+# Fonction pour configurer et installer des paquets essentiels pour l'environnement GNOME.
 function config_gnome() {
     echo -e "|- Configuration de Gnome. ${RED}(long)${RESET}"
-    $AUR_HELPER -S --needed --noconfirm adwaita-icon-theme eog evince file-roller gdm gnome-calculator gnome-console gnome-control-center gnome-disk-utility gnome-keyring gnome-nettool gnome-power-manager gnome-screenshot gnome-shell gnome-text-editor gnome-themes-extra gnome-tweaks gnome-usage gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb nautilus nautilus-sendto sushi totem xdg-user-dirs-gtk gnome-browser-connector adw-gtk3 >> /dev/null 2>&1
+
+    # Liste des paquets GNOME à installer.
+    local packages=(
+        adwaita-icon-theme eog evince file-roller gdm gnome-calculator gnome-console
+        gnome-control-center gnome-disk-utility gnome-keyring gnome-nettool
+        gnome-power-manager gnome-screenshot gnome-shell gnome-text-editor
+        gnome-themes-extra gnome-tweaks gnome-usage gvfs gvfs-afc gvfs-gphoto2
+        gvfs-mtp gvfs-nfs gvfs-smb nautilus nautilus-sendto sushi totem
+        xdg-user-dirs-gtk gnome-browser-connector adw-gtk3
+    )
+
+    # Installe les paquets GNOME listés ci-dessus.
+    $AUR_HELPER -S --needed --noconfirm "${packages[@]}" >> /dev/null 2>&1
+
+    # Définit le thème GTK.
     gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3 >> /dev/null 2>&1
+
+    # Active la touche Num Lock au démarrage.
     gsettings set org.gnome.desktop.peripherals.keyboard numlock-state true >> /dev/null 2>&1    
 }
