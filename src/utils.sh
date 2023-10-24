22c833943cea1d485dd3fb474c915729ec2756da
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-24 20:31:25 +0200

    Update utils.sh

--- src/utils.sh
+++ src/utils.sh
@@ -1,12 +1,14 @@
-
-# Prompts the user with a message and waits for a yes or no answer.
-# Returns 0 if the user answered yes, 1 otherwise.
-# Usage: read_user "Are you sure you want to continue ?"
+# Fonction pour lire la réponse de l'utilisateur, généralement utilisée pour les confirmations.
 function read_user() {
+    # Affiche le message passé en argument suivi de " (y/N) " pour guider l'utilisateur sur comment répondre.
     read -p "${1} (y/N) " answer
+
+    # Vérifie si la réponse de l'utilisateur est "y" ou "Y".
     if [ "$answer" = "Y" ] || [ "$answer" = "y" ]; then
+        # Si l'utilisateur a répondu par l'affirmative, retourne un code de succès (0).
         return 0
     else
+        # Sinon, retourne un code d'échec (1).
         return 1
     fi
 }
