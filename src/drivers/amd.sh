29a2798eccdbcc7e943ccfb6a5051ce19df27ffa
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-24 20:16:12 +0200

    Update amd.sh

--- src/drivers/amd.sh
+++ src/drivers/amd.sh
@@ -1,7 +1,24 @@
-
-# This function installs the necessary drivers for AMD graphics cards.
-# It installs mesa, lib32-mesa, vulkan-radeon, lib32-vulkan-radeon, vulkan-icd-loader, lib32-vulkan-icd-loader, vulkan-mesa-layers, and lib32-vulkan-mesa-layers.
+# Fonction pour installer les pilotes nécessaires pour les cartes graphiques AMD.
 function amd_drivers() {
-    echo "|- Installation de la carte graphique AMD."
-    $AUR_HELPER -S --needed --noconfirm mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader vulkan-mesa-layers lib32-vulkan-mesa-layers >> /dev/null 2>&1
+    echo "|- Installation des pilotes pour la carte graphique AMD."
+
+    # Liste des paquets à installer pour le support des cartes graphiques AMD
+    local amd_packages=(
+        mesa
+        lib32-mesa
+        vulkan-radeon
+        lib32-vulkan-radeon
+        vulkan-icd-loader
+        lib32-vulkan-icd-loader
+        vulkan-mesa-layers
+        lib32-vulkan-mesa-layers
+    )
+
+    # Installation de chaque paquet de la liste
+    for package in "${amd_packages[@]}"; do
+        echo "|- Installation du paquet ${package}."
+        $AUR_HELPER -S --needed --noconfirm "${package}" >> /dev/null 2>&1
+    done
+
+    echo "|- Les pilotes AMD ont été installés avec succès."
 }
