d38623bc1221334672514da9e6e5435de8504374
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-26 21:35:40 +0200

    Update utils.sh

--- src/utils.sh
+++ src/utils.sh
@@ -1,14 +1,7 @@
-# Fonction pour lire la réponse de l'utilisateur, généralement utilisée pour les confirmations.
 function read_user() {
-    # Affiche le message passé en argument suivi de " (y/N) " pour guider l'utilisateur sur comment répondre.
-    read -p "${1} (y/N) " answer
-
-    # Vérifie si la réponse de l'utilisateur est "y" ou "Y".
-    if [ "$answer" = "Y" ] || [ "$answer" = "y" ]; then
-        # Si l'utilisateur a répondu par l'affirmative, retourne un code de succès (0).
-        return 0
-    else
-        # Sinon, retourne un code d'échec (1).
-        return 1
-    fi
+    read -p "${1} [y/N]: " choice
+    case "${choice}" in
+        y|Y ) return 0;;
+        * ) return 1;;
+    esac
 }
