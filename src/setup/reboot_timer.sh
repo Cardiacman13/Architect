cc6b09723142bde5af1d16d8e49607981caafa6c
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-28 22:08:21 +0200

    Update reboot_timer.sh

--- src/setup/reboot_timer.sh
+++ src/setup/reboot_timer.sh
@@ -2,11 +2,11 @@
 function reboot_timer() {
     local timer=$1
 
-    echo -e "${RED}Le script est terminé, le système doit redémarrer${RESET} : Appuyez sur ${GREEN}Entrée${RESET} pour redémarrer ou ${GREEN}Ctrl+C${RESET} pour annuler."
+    echo -e "${GREEN}Le script est terminé, le système doit redémarrer${RESET} : Appuyez sur ${GREEN}Entrée${RESET} pour redémarrer ou ${GREEN}Ctrl+C${RESET} pour annuler."
     read -p "" choice
     if [[ "${choice}" == "" ]]; then
         while [[ ${timer} -gt 0 ]]; do
-            echo -e "${RED}Redémarrage dans ${timer} secondes...${RESET}"
+            echo -e "${GREEN}Redémarrage dans ${timer} secondes...${RESET}"
             sleep 1
             ((timer--))
         done
