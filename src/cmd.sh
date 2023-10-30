efacbf21c846aab738ca8650bbf3763a5c6223ee
wmemcpy <celeste.francier@protonmail.com>
2023-10-30 13:15:45 +0100

    fix number argument cmd

--- src/cmd.sh
+++ src/cmd.sh
@@ -1,27 +1,24 @@
-
 function exec_log() {
-    if [[ $# -ne 3 ]]; then
+    if [[ $# -ne 2 ]]; then
         echo "Usage: exec_log <message> <command>"
         exit 1
     fi
 
     ((EXEC_LOG_COUNT++))
 
-    local -r indent_level="$1"
-    local -r command="$2"
-    local -r comment="$3"
+    local -r command="$1"
+    local -r comment="$2"
 
     echo -e "${comment}"
     echo "[$(date "+%Y-%m-%d %H:%M:%S")] ${comment}" >>"${LOG_FILE}"
 
     if [[ ${VERBOSE} == true ]]; then
-        # print command output with the error output in red, and put in to log file
-        { 
-            eval "$(command)" 2>&1 1>&3 | 
-            while read -r line; do 
-                ((ERROR_COUNT++))
-                echo -e "${RED}${line}${RESET}"
-            done
+        {
+            eval "$(command)" 2>&1 1>&3 |
+                while read -r line; do
+                    ((ERROR_COUNT++))
+                    echo -e "${RED}${line}${RESET}"
+                done
         } 3>&1 1>&2 | tee -a "${LOG_FILE}"
     else
         eval "$(command)" >>"${LOG_FILE}" 2>&1 >/dev/null
