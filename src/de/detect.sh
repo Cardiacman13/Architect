23c1ff2f54cf023400e79743654e4a1d733b02e1
wmemcpy <celeste.francier@protonmail.com>
2023-10-30 21:44:30 +0100

    wrong question

--- src/de/detect.sh
+++ src/de/detect.sh
@@ -5,10 +5,10 @@ source src/de/xfce.sh
 function detect_de() {
     local -r valid_des="GNOME KDE XFCE"
 
-    read -rp "What is your graphics card type ? (${valid_des}) : " choice
+    read -rp "What is your desktop environment (${valid_des}) : " choice
     choice="${choice^^}"
     while [[ ! ${valid_des} =~ (^|[[:space:]])"${choice}"($|[[:space:]]) ]]; do
-        read -rp "What is your graphics card type ? (${valid_des}) : " choice
+        read -rp "What is your desktop environment ? (${valid_des}) : " choice
         choice="${choice^^}"
     done
 
