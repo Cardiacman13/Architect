089020d3d0fde25f8e54f177e83878bbe4fc50e4
wmemcpy <celeste.francier@protonmail.com>
2023-11-01 18:59:32 +0100

    revert to f3b1d4a966af66740b02b012d26909303e173cec

--- src/software/flatpak.sh
+++
@@ -1,6 +0,0 @@
-source src/cmd.sh
-
-function support_flatpak() {
-    install_sp "flatpak"
-    exec_log "flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo" "add flathub repository"
-}
