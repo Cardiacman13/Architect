e9f01268fda9de96c515f8f5f35955808f6539a7
wmemcpy <celeste.francier@protonmail.com>
2023-11-05 20:34:11 +0100

    Update pacman configuration and mirrorlist

--- src/system/config/pacman.sh
+++ src/system/config/pacman.sh
@@ -5,11 +5,11 @@ function config_pacman() {
     exec_log "sudo sed -i 's/^#VerbosePkgLists$/VerbosePkgLists/' '/etc/pacman.conf'" "Enabling verbose package lists in pacman"
     exec_log "sudo sed -i 's/^#\(ParallelDownloads.*\)/\1/' '/etc/pacman.conf'" "Enabling parallel downloads in pacman"
     exec_log "sudo sed -i '/^#\[multilib\]/,/^#Include = \/etc\/pacman.d\/mirrorlist/ s/^#//' '/etc/pacman.conf'" "Enabling multilib repository"
-    exec_log "sudo pacman --noconfirm -Syu" "updating full system"
+    exec_log "sudo pacman --noconfirm -Syu" "Updating full system"
 }
 
 function mirrorlist() {
     install_one "reflector"
-    exec_log "sudo reflector --verbose --score 20 --fastest 5 --sort rate --save /etc/pacman.d/mirrorlist" "updating mirrorlist"
-    exec_log "sudo pacman -Syy" "updating pacman database"
+    exec_log "sudo reflector --verbose --score 20 --fastest 5 --sort rate --save /etc/pacman.d/mirrorlist" "Updating mirrorlist"
+    exec_log "sudo pacman -Syy" "Updating pacman database"
 }
