8b66b768610e6ada804c65c56ef10ece27866f0c
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-11-15 14:51:52 +0100

    Update pacman.sh

--- src/system/config/pacman.sh
+++ src/system/config/pacman.sh
@@ -5,8 +5,8 @@ function config_pacman() {
     exec_log "sudo sed -i 's/^#VerbosePkgLists$/VerbosePkgLists/' '/etc/pacman.conf'" "Enabling verbose package lists in pacman"
     exec_log "sudo sed -i 's/^#\(ParallelDownloads.*\)/\1/' '/etc/pacman.conf'" "Enabling parallel downloads in pacman"
     exec_log "sudo sed -i '/^#\[multilib\]/,/^#Include = \/etc\/pacman.d\/mirrorlist/ s/^#//' '/etc/pacman.conf'" "Enabling multilib repository"
-    exec_log "sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS=-j$(nproc)/' /etc/makepkg.conf"
-    exec_log "sudo pacman --noconfirm -Syu" "Updating full system" "Enabling multithread compilation"
+    exec_log "sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS=-j$(nproc)/' /etc/makepkg.conf" "Enabling multithread compilation"
+    exec_log "sudo pacman --noconfirm -Syu" "Updating full system"
 }
 
 function mirrorlist() {
