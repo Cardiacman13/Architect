4b5e557470692ef75fffb2fe9adcb0a23b3e9e2b
wmemcpy <celeste.francier@protonmail.com>
2023-11-18 17:12:48 +0100

    Update software installation script

--- src/software/install.sh
+++ src/software/install.sh
@@ -11,26 +11,6 @@ function ask_to_add() {
     fi
 }
 
-# Asking to user if he want to install some software
-# | type    | name                    | package                                 |
-# |---------|-------------------------|-----------------------------------------|
-# | package | Firefox                 | firefox firefox-i18n-fr                 |
-# | package | Brave                   | brave-bin                               |
-# | package | Chromium                | chromium                                |
-# | package | Discord                 | discord                                 |
-# | package | Steam                   | steam                                   |
-# | package | Lutris                  | lutris wine-staging                     |
-# | package | Heroic Games Launcher   | heroic-games-launcher-bin               |
-# | package | protonup-qt             | protonup-qt-bin                         |
-# | package | Spotify                 | spotify                                 |
-# | flatpak | OBS Studio              | com.obsproject.Studio                   |
-# | package | Kdenlive                | kdenlive                                |
-# | package | LibreOffice             | libreoffice-fresh libreoffice-fresh-fr  |
-# | package | Gimp                    | gimp                                    |
-# | package | VLC                     | vlc                                     |
-# | package | Visual Studio Code      | visual-studio-code-bin                  |
-# | package | Open RGB                | openrgb-bin                             |
-# |-----------------------------------------------------------------------------|
 function install_software() {
     local flatpak=$(ask_to_add "OBS Studio (flatpak)" "com.obsproject.Studio")
     local packages=$(ask_to_add "Firefox" "firefox firefox-i18n-fr")
