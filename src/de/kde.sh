23f5ac1e03163ecdc5460a5a4a5846f86380f0a0
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-11-19 16:12:22 +0100

    Update kde.sh

--- src/de/kde.sh
+++ src/de/kde.sh
@@ -29,5 +29,14 @@ function install_kde() {
     "
 
     install_lst "${inlst}"
-    exec_log "sudo echo "Numlock=on" >> /etc/sddm.conf" "Enabling numlock on startup"
+
+    if [ ! -f /etc/sddm.conf ]; then
+        exec_log "sudo touch /etc/sddm.conf" "Creating /etc/sddm.conf"
+    fi
+
+    exec_log "sudo sed -i '/Numlock=/d' /etc/sddm.conf" "Removing existing Numlock entry if any"
+    exec_log "echo 'Numlock=on' | sudo tee -a /etc/sddm.conf" "Enabling numlock on startup"
+
+    exec_log "sudo sed -i '/Current=/d' /etc/sddm.conf" "Removing existing Current theme entry if any"
+    exec_log "echo 'Current=breeze' | sudo tee -a /etc/sddm.conf" "Enabling Breeze SDDM theme"
 }
