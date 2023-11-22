ec904827b74d1dfc867b32ae010f1ef43914b386
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-11-22 17:33:14 +0100

    fix: unlock Wayland on Gnome for Nvidia
    add : sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules to unlock Wayland choice on GDM, if they want, they can switch back to X11 on GDM too.

--- src/de/gnome.sh
+++ src/de/gnome.sh
@@ -39,4 +39,5 @@ function install_gnome() {
     install_lst "${inlst}"
     exec_log "gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3" "Setting gtk theme to adw-gtk3"
     exec_log "gsettings set org.gnome.desktop.peripherals.keyboard numlock-state true" "Enabling numlock on startup"
+    exec_log "sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules" "Disable GDM rules to unlock Wayland"
 }
