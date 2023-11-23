a7d3d08792b8f67762c85ab6becee24bbda8e8d4
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-11-23 19:36:05 +0100

    Fix: Add ILovaCandy
    Best commit ever

--- src/system/config/pacman.sh
+++ src/system/config/pacman.sh
@@ -3,7 +3,7 @@ source src/cmd.sh
 function config_pacman() {
     exec_log "sudo sed -i 's/^#Color$/Color/' '/etc/pacman.conf'" "Enabling color in pacman"
     exec_log "sudo sed -i 's/^#VerbosePkgLists$/VerbosePkgLists/' '/etc/pacman.conf'" "Enabling verbose package lists in pacman"
-    exec_log "sudo sed -i 's/^#\(ParallelDownloads.*\)/\1/' '/etc/pacman.conf'" "Enabling parallel downloads in pacman"
+    exec_log "sudo sed -i 's/^#\(ParallelDownloads.*\)/\1\nILoveCandy/' '/etc/pacman.conf'" "Enabling parallel downloads and ILoveCandy in pacman"
     exec_log "sudo sed -i '/^#\[multilib\]/,/^#Include = \/etc\/pacman.d\/mirrorlist/ s/^#//' '/etc/pacman.conf'" "Enabling multilib repository"
     exec_log "sudo sed -i 's/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j\$(nproc)\"/' /etc/makepkg.conf" "Enabling multithread compilation"
     exec_log "sudo pacman --noconfirm -Syu" "Updating full system"
