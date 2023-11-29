75899b0efd76daa0de89d7418ce36ba2056ac95b
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-11-29 14:16:24 +0100

    fix: nvidia drm

--- src/system/drivers/nvidia.sh
+++ src/system/drivers/nvidia.sh
@@ -1,9 +1,7 @@
 source src/cmd.sh
 
 function nvidia_config() {
-    if ! exec_log "grep -q "options nvidia-drm.modeset=1" /etc/modprobe.d/nvidia.conf" "Checking nvidia-drm.modeset=1 option"; then
         exec_log "echo -e 'options nvidia-drm.modeset=1' | sudo tee -a /etc/modprobe.d/nvidia.conf" "Setting nvidia-drm.modeset=1 option"
-    fi
 }
 
 function nvidia_drivers() {
