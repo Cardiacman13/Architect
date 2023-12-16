3c36678b1638d1dfa6420cf628d85c19907f72c4
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-12-16 20:00:19 +0100

    fix replace snapper with timeshift for grub-btrfs

--- src/system/grub-btrfs.sh
+++ src/system/grub-btrfs.sh
@@ -1,13 +1,11 @@
 function grub-btrfs() {
-    read -rp "Do you want to install and setup grub-btrfs ${RED}say No if unsure${RESET} /!\  ? (y/N) : " choice
+    read -rp "Do you want to install and setup grub-btrfs and timeshift ${RED}say No if unsure${RESET} /!\  ? (y/N) : " choice
     choice="${choice,,}"
 
     if [[ $choice =~ ^(yes|y)$ ]]; then
-        install_lst "snapper snapper-support snap-pac grub-btrfs"
-        exec_log "sudo umount /.snapshots" "Umount /.snapshots"
-        exec_log "sudo rm -r /.snapshots" "Remove /.snapshots"
-        exec_log "sudo snapper -c root create-config /" "Snapper create-config for root in /.snapshots"
-        exec_log "sudo systemctl enable --now grub-btrfsd" "Enable and start grub-btrfsd service"
-        exec_log "sudo btrfs subvol set-default 256 /" "set-default subvolume 256"
+        install_lst "timeshift grub-btrfs timeshift-autosnap"
+        exec_log "sudo systemctl enable --now cronie.service" "Enable cronie"
+        exec_log "sudo systemctl enable --now grub-btrfsd" "Enable grub-btrfsd"
+        exec_log "sudo sed -i 's|ExecStart=/usr/bin/grub-btrfsd --syslog /.snapshots|ExecStart=/usr/bin/grub-btrfsd --syslog --timeshift-auto|' /usr/lib/systemd/system/grub-btrfsd.service" "setup grub-btrfsd for timeshift"
     fi
 }
