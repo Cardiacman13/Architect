918763ff38368fa509714da48f7b12b2105e6bf6
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2023-12-19 14:38:46 +0100

    Change target of sed command grub-btrfs.sh
    Change target of sed command, from /usr/lib/systemd/system, which is a generated file, to /etc/systemd/system

--- src/system/grub-btrfs.sh
+++ src/system/grub-btrfs.sh
@@ -6,6 +6,6 @@ function grub-btrfs() {
         install_lst "timeshift grub-btrfs timeshift-autosnap"
         exec_log "sudo systemctl enable --now cronie.service" "Enable cronie"
         exec_log "sudo systemctl enable --now grub-btrfsd" "Enable grub-btrfsd"
-        exec_log "sudo sed -i 's|ExecStart=/usr/bin/grub-btrfsd --syslog /.snapshots|ExecStart=/usr/bin/grub-btrfsd --syslog --timeshift-auto|' /usr/lib/systemd/system/grub-btrfsd.service" "setup grub-btrfsd for timeshift"
+        exec_log "sudo sed -i 's|ExecStart=/usr/bin/grub-btrfsd --syslog /.snapshots|ExecStart=/usr/bin/grub-btrfsd --syslog --timeshift-auto|' /etc/systemd/system/multi-user.target.wants/grub-btrfsd.service" "setup grub-btrfsd for timeshift"
     fi
 }
