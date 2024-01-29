9c37e22d6de96ffe51a07d9413de7d5fe0e9e0ae
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2024-01-29 16:45:40 +0100

    fix: enable paccache.timer

--- src/system/config/pacman.sh
+++ src/system/config/pacman.sh
@@ -6,6 +6,7 @@ function config_pacman() {
     exec_log "sudo sed -i 's/^#\(ParallelDownloads.*\)/\1\nILoveCandy/' '/etc/pacman.conf'" "$(eval_gettext "Enabling parallel downloads and ILoveCandy in pacman")"
     exec_log "sudo sed -i '/^#\[multilib\]/,/^#Include = \/etc\/pacman.d\/mirrorlist/ s/^#//' '/etc/pacman.conf'" "$(eval_gettext "Enabling multilib repository")"
     exec_log "sudo sed -i 's/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j\$(nproc)\"/' /etc/makepkg.conf" "$(eval_gettext "Enabling multithread compilation")"
+    exec_log "sudo pacman -S pacman-contrib --noconfirm" "$(eval_gettext "Installing pacman-contrib")"
     exec_log "sudo systemctl enable paccache.timer" "$(eval_gettext "Enabling paccache timer")"
 }
 
@@ -13,5 +14,4 @@ function mirrorlist() {
     install_one "reflector"
     exec_log "sudo reflector --verbose --score 100 --latest 20 --fastest 5 --sort rate --save /etc/pacman.d/mirrorlist" "$(eval_gettext "Updating mirrorlist \${RED}(might be long)\${RESET}")"
     exec_log "sudo pacman -Syyu --noconfirm" "$(eval_gettext "Updating full system \${RED}(might be long)\${RESET}")"
-    exec_log "sudo pacman -S pacman-contrib --noconfirm" "$(eval_gettext "Installing pacman-contrib")"
 }
