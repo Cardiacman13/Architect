14744d4dc0eef14cb634e5f0bce0401261a71607
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2024-03-03 19:20:43 +0100

    refresh mirros after add multilib

--- src/system/config/pacman.sh
+++ src/system/config/pacman.sh
@@ -8,6 +8,7 @@ function config_pacman() {
     exec_log "sudo sed -i 's/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j\$(nproc)\"/' /etc/makepkg.conf" "$(eval_gettext "Enabling multithread compilation")"
     exec_log "sudo pacman -S pacman-contrib --noconfirm" "$(eval_gettext "Installing pacman-contrib")"
     exec_log "sudo systemctl enable paccache.timer" "$(eval_gettext "Enabling paccache timer")"
+    exec_log "sudo pacman -Syyu --noconfirm" "$(eval_gettext "Updating full system \${RED}(might be long)\${RESET}")"
 }
 
 function mirrorlist() {
