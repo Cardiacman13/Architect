a50d0bc5bc19434cffba75ac13d30c7a43077c7e
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2024-03-10 18:54:53 +0100

    fbdev=1 + early loading

--- src/system/drivers/nvidia.sh
+++ src/system/drivers/nvidia.sh
@@ -2,7 +2,8 @@ source src/cmd.sh
 
 function nvidia_config() {
     exec_log "echo -e 'options nvidia NVreg_UsePageAttributeTable=1 NVreg_InitializeSystemMemoryAllocations=0 NVreg_DynamicPowerManagement=0x02' | sudo tee -a /etc/modprobe.d/nvidia.conf" "$(eval_gettext "Setting nvidia power management option")"
-    exec_log "echo -e 'options nvidia_drm modeset=1' | sudo tee -a /etc/modprobe.d/nvidia.conf" "$(eval_gettext "Setting nvidia-drm modeset=1 option")"
+    exec_log "echo -e 'options nvidia_drm modeset=1 fbdev=1' | sudo tee -a /etc/modprobe.d/nvidia.conf" "$(eval_gettext "Setting nvidia-drm modeset=1 option")"
+    exec_log "sudo sed -i "/^MODULES=(/ s/)$/ nvidia nvidia_modeset nvidia_uvm nvidia_drm)/" /etc/mkinitcpio.conf" "$(eval_gettext "Setting early loading")"
 }
 
 function nvidia_intel() {
