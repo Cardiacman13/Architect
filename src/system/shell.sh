306eded09f2f8565420d75f717e57a83157cc7f7
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2024-03-10 18:52:13 +0100

    add sudo to update grub

--- src/system/shell.sh
+++ src/system/shell.sh
@@ -10,7 +10,7 @@ function shell_config() {
         "alias fix-key='sudo rm /var/lib/pacman/sync/* && sudo rm -rf /etc/pacman.d/gnupg/* && sudo pacman-key --init && sudo pacman-key --populate && sudo pacman -Sy --noconfirm archlinux-keyring && sudo pacman --noconfirm -Su'"
         "alias update-arch='${AUR}'"
         "alias update-mirrors='sudo reflector --verbose --score 100 --latest 20 --fastest 5 --sort rate --save /etc/pacman.d/mirrorlist && sudo pacman -Syyu'"
-        "alias update-grub='grub-mkconfig -o /boot/grub/grub.cfg'"
+        "alias update-grub='sudo grub-mkconfig -o /boot/grub/grub.cfg'"
     )
 
     if [[ ${AUR} == yay ]]; then
