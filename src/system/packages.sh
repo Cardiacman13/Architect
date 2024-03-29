ba5013ffc54fcea1bf39819e71613bc5bf1a8aa4
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2024-03-29 14:26:29 +0100

    remove zram
    remove zram because archinstall do it now

--- src/system/packages.sh
+++ src/system/packages.sh
@@ -45,9 +45,7 @@ function usefull_package() {
         ffmpegthumbs
         man-db
         man-pages
-        zram-generator
     "
-    exec_log "echo -e '[zram0]\nzram-size = ram / 4\ncompression-algorithm = zstd\nswap-priority = 100\nfs-type = swap' | sudo tee -a /etc/systemd/zram-generator.conf" "$(eval_gettext "Enabling Zram")"
     
     exec_log "echo -e 'vm.swappiness=10' | sudo tee /etc/sysctl.d/99-swappiness.conf" "$(eval_gettext "Setting swappiness to 10")"
 
