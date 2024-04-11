2e278eb58dc6c975fb30fc21872eae9c020d325a
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2024-04-11 12:45:15 +0000

    Update nvidia.sh

--- src/system/drivers/nvidia.sh
+++ src/system/drivers/nvidia.sh
@@ -83,7 +83,6 @@ exec_log "sudo systemctl enable nvidia-suspend.service nvidia-hibernate.service 
 echo "GTK_USE_PORTAL=1
 GBM_BACKEND=nvidia-drm
 __GLX_VENDOR_LIBRARY_NAME=nvidia
-QT_QPA_PLATFORM=wayland
 ELECTRON_OZONE_PLATFORM_HINT=auto" | sudo tee -a /etc/environement
 
 }
