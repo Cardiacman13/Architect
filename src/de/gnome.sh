4c82265f9d99bc57b53f45d5be2edbc8931a7d2e
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2025-04-03 00:52:27 +0200

    Add "gnome-software" to IgnorePkg

--- src/de/gnome.sh
+++ src/de/gnome.sh
@@ -49,4 +49,23 @@ function install_gnome() {
 
     # Disable GDM rules to unlock Wayland
     exec_log "sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules" "$(eval_gettext "Disable GDM rules to unlock Wayland")"
+
+    # Ensure 'gnome-software' is added to the IgnorePkg line in /etc/pacman.conf
+    #  1. If the line is commented (#IgnorePkg = ...), uncomment and add 'gnome-software' if missing
+    #  2. If the line is already uncommented (IgnorePkg = ...), add 'gnome-software' if missing
+    #  3. If the line does not exist at all, append "IgnorePkg = gnome-software" at the end of the file
+    exec_log \
+        "sed -i -E '
+          /^[[:space:]]*#[[:space:]]*IgnorePkg[[:space:]]*=/ {
+            s/^[[:space:]]*#[[:space:]]*//             # Remove the # and any leading spaces
+            /(\s|^)gnome-software(\s|$)/! s/$/ gnome-software/   # Add gnome-software if it is not already present
+            b
+          }
+          /^[[:space:]]*IgnorePkg[[:space:]]*=/ {
+            /(\s|^)gnome-software(\s|$)/! s/$/ gnome-software/   # Add gnome-software if missing
+            b
+          }
+          $ aIgnorePkg = gnome-software                # If no line found, append at EOF
+        ' /etc/pacman.conf" \
+        "$(eval_gettext "Adding 'gnome-software' to IgnorePkg in /etc/pacman.conf")"
 }
