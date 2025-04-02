cc8b5be7b8efafea67da9c7c1d307ca4582a9103
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2025-04-02 20:12:26 +0200

    Silence firewall-cmd output in printer.sh for cleaner terminal output
    - Redirect all firewall-cmd commands to /dev/null
    - Ensures a cleaner, more user-friendly installation experience

--- src/system/firewall.sh
+++ src/system/firewall.sh
@@ -5,8 +5,8 @@ source src/cmd.sh
 function firewall() {
     if ask_question "$(eval_gettext "Do you want to install a firewall /!\\ Install and activate firewalld? The default configuration may block access to printers and other devices on your local network ?")"; then
         install_lst "firewalld python-pyqt5 python-capng"
-        exec_log "sudo systemctl enable --now firewalld.service" "$(eval_gettext "Enabling firewalld")"
-        exec_log "sudo firewall-cmd --remove-service="ssh" --permanent" "$(eval_gettext "Remove the default authorised ssh service")"
-        exec_log "sudo firewall-cmd --remove-service="dhcpv6-client" --permanent" "$(eval_gettext "Remove the default authorised DHCPV6-client service")"
+        sudo systemctl enable --now firewalld.service &> /dev/null
+        sudo firewall-cmd --remove-service="ssh" --permanent &> /dev/null
+        sudo firewall-cmd --remove-service="dhcpv6-client" --permanent &> /dev/null
     fi
 }
