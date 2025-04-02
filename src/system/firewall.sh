75c8100310301f0952ca8b931b2c23663ae96450
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2025-04-02 17:24:45 +0200

    remove firewalld default rules

--- src/system/firewall.sh
+++ src/system/firewall.sh
@@ -6,5 +6,7 @@ function firewall() {
     if ask_question "$(eval_gettext "Do you want to install a firewall /!\\ Install and activate firewalld? The default configuration may block access to printers and other devices on your local network ?")"; then
         install_lst "firewalld python-pyqt5 python-capng"
         exec_log "sudo systemctl enable --now firewalld.service" "$(eval_gettext "Enabling firewalld")"
+        exec_log "sudo firewall-cmd --remove-service="ssh" --permanent" "$(eval_gettext "Remove the default authorised ssh service")"
+        exec_log "sudo firewall-cmd --remove-service="dhcpv6-client" --permanent" "$(eval_gettext "Remove the default authorised DHCPV6-client service")"
     fi
 }
