bafedcb1eae088213c6a3742f8fb6cc32c465af2
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2025-04-02 18:08:28 +0200

    add com

--- src/de/gnome.sh
+++ src/de/gnome.sh
@@ -1,6 +1,9 @@
+# Source the script from src/cmd.sh
 source src/cmd.sh
 
+# Function to install GNOME applications
 function install_gnome() {
+    # Define a list of GNOME applications to install
     local -r inlst="
         gnome
         gnome-tweaks
@@ -35,9 +38,15 @@ function install_gnome() {
         qt6-wayland
     "
 
+    # Call the install_lst function to install the listed applications
     install_lst "${inlst}"
 
+    # Set the GTK theme to adw-gtk3
     exec_log "gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3" "$(eval_gettext "Setting gtk theme to adw-gtk3")"
+
+    # Enable Numlock on startup
     exec_log "gsettings set org.gnome.desktop.peripherals.keyboard numlock-state true" "$(eval_gettext "Enabling numlock on startup")"
+
+    # Disable GDM rules to unlock Wayland
     exec_log "sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules" "$(eval_gettext "Disable GDM rules to unlock Wayland")"
 }
