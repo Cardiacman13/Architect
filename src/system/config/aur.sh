416cd358bafa0b878ed7546f39c8042f7ef46986
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2025-04-03 09:55:43 +0200

    improve aur function
    - Check if git is installed before installing
    - Use a case statement for cleaner logic (yay vs paru)
    - Exit early if cd fails
    - Remove cloned repository after installation
    - Display messages if AUR helper is already installed
    - Separate post-install steps for yay and paru

--- src/system/config/aur.sh
+++ src/system/config/aur.sh
@@ -1,9 +1,13 @@
 source src/cmd.sh
 
 function install_aur() {
-    # Install git if not already installed (needed to clone AUR helper repos)
-    exec_log "sudo pacman -S --noconfirm --needed git" "$(eval_gettext "Installing git")"
+    # Check if git is installed; if not, install it
+    if ! command -v git &>/dev/null; then
+        exec_log "sudo pacman -S --noconfirm --needed git" \
+                 "$(eval_gettext "Installing git")"
+    fi
 
+    # Define AUR helper Git URLs and package names
     local -r git_url=(
         "https://aur.archlinux.org/yay-bin.git"
         "https://aur.archlinux.org/paru-bin.git"
@@ -12,50 +16,71 @@ function install_aur() {
         "yay-bin"
         "paru-bin"
     )
-    local id=-1
-    local choice=""
 
-    # Prompt user for AUR helper choice
+    # Prompt the user to choose which AUR helper to install
+    local choice=""
     while [[ $choice != "yay" && $choice != "paru" ]]; do
-        read -rp "$(eval_gettext "which aur helper do you want to install ? (yay/paru) : ")" choice
-        choice="${choice,,}"
+        read -rp "$(eval_gettext "Which AUR helper do you want to install? (yay/paru): ")" choice
+        choice="${choice,,}"  # Convert to lowercase
     done
 
-    eval_gettext "\${GREEN}You chose \${choice}\${RESET}"; echo
+    eval_gettext "\${GREEN}You chose \${choice}\${RESET}"
+    echo
 
-    # Set AUR variable regardless of installation status
-    if [[ $choice == "yay" ]]; then
-        id=0
-        export AUR="yay"
-    elif [[ $choice == "paru" ]]; then
-        id=1
-        export AUR="paru"
-    fi
+    # Set the index and global $AUR variable based on the choice
+    local helper_index
+    case "$choice" in
+        yay)
+            helper_index=0
+            export AUR="yay"
+            ;;
+        paru)
+            helper_index=1
+            export AUR="paru"
+            ;;
+    esac
 
-    # Install AUR helper only if not already installed
+    # Install the chosen AUR helper if it is not already installed
     if ! pacman -Qi "${AUR}" &>/dev/null; then
-        DIR=${aur_name[$id]}
-        exec_log "git clone ${git_url[$id]}" "$(eval_gettext "Cloning $DIR")"
-        cd "$DIR" || return 1
-        exec_log "makepkg -si --noconfirm" "$(eval_gettext "Installing \${AUR}")"
+        local dir="${aur_name[$helper_index]}"
+        exec_log "git clone ${git_url[$helper_index]}" \
+                 "$(eval_gettext "Cloning ${dir}")"
+        cd "${dir}" || return 1
+        exec_log "makepkg -si --noconfirm" \
+                 "$(eval_gettext "Installing ${AUR}")"
         cd .. || return 1
-        exec_log "rm -rf $DIR" "$(eval_gettext "Deleting directory $DIR")"
+        exec_log "rm -rf \"${dir}\"" \
+                 "$(eval_gettext "Deleting directory ${dir}")"
+    else
+        eval_gettext "\${AUR} is already installed. Skipping build."
+        echo
     fi
 
-    # Post-installation configuration for yay
-    if [[ $AUR == "yay" ]]; then
-        exec_log "yay -Y --gendb" "$(eval_gettext "Configuring \${AUR}")"
-        exec_log "yay -Y --devel --save" "$(eval_gettext "Configuring \${AUR}")"
-        exec_log "sed -i 's/\"sudoloop\": false,/\"sudoloop\": true,/' ~/.config/yay/config.json" "$(eval_gettext "Enabling SudoLoop option for yay")"
-    
-    # Post-installation configuration for paru
-    elif [[ $AUR == "paru" ]]; then
-        exec_log "paru --gendb" "$(eval_gettext "Configuring \${AUR}")"
-        exec_log "sudo sed -i 's/#BottomUp/BottomUp/' /etc/paru.conf" "$(eval_gettext "Enabling BottomUp option for paru")"
-        exec_log "sudo sed -i 's/#SudoLoop/SudoLoop/' /etc/paru.conf" "$(eval_gettext "Enabling SudoLoop option for paru")"
-        exec_log "sudo sed -i 's/#CombinedUpgrade/CombinedUpgrade/' /etc/paru.conf" "$(eval_gettext "Enabling CombinedUpgrade option for paru")"
-        exec_log "sudo sed -i 's/#UpgradeMenu/UpgradeMenu/' /etc/paru.conf" "$(eval_gettext "Enabling UpgradeMenu option for paru")"
-        exec_log "sudo sed -i 's/#NewsOnUpgrade/NewsOnUpgrade/' /etc/paru.conf" "$(eval_gettext "Enabling NewsOnUpgrade option for paru")"
-        exec_log "sudo sh -c 'echo "SkipReview" >> /etc/paru.conf'" "$(eval_gettext "Enabling SkipReview option for paru")"
-    fi
+    # Post-installation configuration
+    case "$AUR" in
+        yay)
+            exec_log "yay -Y --gendb" \
+                     "$(eval_gettext "Configuring ${AUR}")"
+            exec_log "yay -Y --devel --save" \
+                     "$(eval_gettext "Configuring ${AUR}")"
+            exec_log "sed -i 's/\"sudoloop\": false,/\"sudoloop\": true,/' ~/.config/yay/config.json" \
+                     "$(eval_gettext "Enabling SudoLoop option for yay")"
+            ;;
+        paru)
+            exec_log "paru --gendb" \
+                     "$(eval_gettext "Configuring ${AUR}")"
+            exec_log "sudo sed -i 's/#BottomUp/BottomUp/' /etc/paru.conf" \
+                     "$(eval_gettext "Enabling BottomUp option for paru")"
+            exec_log "sudo sed -i 's/#SudoLoop/SudoLoop/' /etc/paru.conf" \
+                     "$(eval_gettext "Enabling SudoLoop option for paru")"
+            exec_log "sudo sed -i 's/#CombinedUpgrade/CombinedUpgrade/' /etc/paru.conf" \
+                     "$(eval_gettext "Enabling CombinedUpgrade option for paru")"
+            exec_log "sudo sed -i 's/#UpgradeMenu/UpgradeMenu/' /etc/paru.conf" \
+                     "$(eval_gettext "Enabling UpgradeMenu option for paru")"
+            exec_log "sudo sed -i 's/#NewsOnUpgrade/NewsOnUpgrade/' /etc/paru.conf" \
+                     "$(eval_gettext "Enabling NewsOnUpgrade option for paru")"
+            exec_log "sudo sh -c 'echo SkipReview >> /etc/paru.conf'" \
+                     "$(eval_gettext "Enabling SkipReview option for paru")"
+            ;;
+    esac
 }
