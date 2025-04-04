8adb0001727a2f6c4939a78431fc609d2d9f207e
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2025-04-04 23:20:09 +0200

    improve shell configuration function and alias handling
    - Refactored `shell_config` function for better readability and maintainability
    - Added English comments for clarity
    - Renamed variables for consistency and to avoid conflicts with Bash built-ins
    - Ensured aliases are added safely to selected shell config files
    - Handled default shell selection more robustly
    - Ensured default aliases are always added to .bashrc for fallback

--- src/system/shell.sh
+++ src/system/shell.sh
@@ -1,101 +1,100 @@
-# Load shared functions
+# Load shared utility functions
 source src/cmd.sh
 
-# Configure default shell and useful aliases based on user's shell preference
+# Function to configure the default shell and add useful aliases based on user preference
 function shell_config() {
-    # List of shell config files (bash, zsh, fish)
+    # Define the paths to configuration files for each shell
     local -r config_file=(
         "${HOME}/.bashrc"
         "${HOME}/.zshrc"
         "${HOME}/.config/fish/config.fish"
     )
 
-    # Define commonly useful aliases
-    local alias=(
-        "alias fix-key='sudo rm /var/lib/pacman/sync/* && sudo rm -rf /etc/pacman.d/gnupg/* && sudo pacman-key --init && sudo pacman-key --populate && sudo pacman -Sy --noconfirm archlinux-keyring && sudo pacman --noconfirm -Su'",
-        "alias update-arch='${AUR}'",
-        "alias update-grub='sudo grub-mkconfig -o /boot/grub/grub.cfg'",
-        "alias reinstall-all-pkg='sudo pacman -S  \$(pacman -Qnq)'"
+    # Define a list of helpful shell aliases
+    local alias_list=(
+        "alias fix-key='sudo rm /var/lib/pacman/sync/* && sudo rm -rf /etc/pacman.d/gnupg/* && sudo pacman-key --init && sudo pacman-key --populate && sudo pacman -Sy --noconfirm archlinux-keyring && sudo pacman --noconfirm -Su'"
+        "alias update-arch='${AUR}'"
+        "alias update-grub='sudo grub-mkconfig -o /boot/grub/grub.cfg'"
+        "alias reinstall-all-pkg='sudo pacman -S \$(pacman -Qnq)'"
     )
 
-    # Add clean-arch alias based on AUR helper
+    # Add a cleanup alias based on the selected AUR helper
     if [[ ${AUR} == yay ]]; then
-        alias+=("alias clean-arch='yay -Sc && yay -Yc'")
+        alias_list+=("alias clean-arch='yay -Sc && yay -Yc'")
     elif [[ ${AUR} == paru ]]; then
-        alias+=("alias clean-arch='paru -Sc && paru -c'")
+        alias_list+=("alias clean-arch='paru -Sc && paru -c'")
     fi
 
-    # Add aliases to .bashrc to ensure availability even if shell is not changed
-    for i in "${alias[@]}"; do
-        if ! grep -q "${i}" "${HOME}/.bashrc"; then
-            echo "${i}" >>"${HOME}/.bashrc"
+    # Always add aliases to .bashrc so they are accessible even if the user doesn't switch shell
+    for alias_cmd in "${alias_list[@]}"; do
+        if ! grep -q "${alias_cmd}" "${HOME}/.bashrc"; then
+            echo "${alias_cmd}" >> "${HOME}/.bashrc"
         fi
     done
 
-    # Ask the user what their default shell is
+    # Ask the user to choose their default shell (bash, zsh, or fish)
     local index=0
     local choice=""
 
     while [[ $choice != "bash" && $choice != "zsh" && $choice != "fish" ]]; do
         read -rp "$(eval_gettext "What is your default shell ? (bash/zsh/fish) : ")" choice
-        choice="${choice,,}"
+        choice="${choice,,}"  # Convert to lowercase
     done
 
     eval_gettext "\${GREEN}You chose \${choice}\${RESET}"; echo
 
-    # Configure specific shell environment and change default shell if needed
+    # Configure shell environment and change default shell if needed
     case $choice in
-    bash)
-        touch "${HOME}/.bashrc"
-        ;;
-    zsh)
-        install_one "zsh"
-        install_one "zsh-completions"
+        bash)
+            touch "${HOME}/.bashrc"
+            index=0
+            ;;
+        zsh)
+            install_one "zsh"
+            install_one "zsh-completions"
 
-        local current_shell=$(getent passwd $USER | cut -d: -f7)
+            local current_shell=$(getent passwd "$USER" | cut -d: -f7)
+            while [[ "$current_shell" != "/usr/bin/zsh" ]]; do
+                eval_gettext "Setting default shell to zsh..."; echo
+                chsh -s "/usr/bin/zsh"
+                current_shell=$(getent passwd "$USER" | cut -d: -f7)
+            done
 
-        while [ "$current_shell" != "/usr/bin/zsh" ]; do
-            eval_gettext "Setting default shell to zsh..."; echo
-            chsh -s "/usr/bin/zsh"
-            current_shell=$(getent passwd $USER | cut -d: -f7)
-        done
+            if ask_question "$(eval_gettext "Do you want to install oh-my-zsh ? This will install the default oh-my-zsh's .zshrc configuration")"; then
+                eval_gettext "You chose to install oh-my-zsh"; echo
+                git clone https://github.com/ohmyzsh/ohmyzsh.git "${HOME}/.oh-my-zsh"
+                cp "${HOME}/.oh-my-zsh/templates/zshrc.zsh-template" "${HOME}/.zshrc"
+            fi
 
-        if ask_question "$(eval_gettext "Do you want to install oh-my-zsh ? This will install the default oh-my-zsh's .zshrc configuration")"; then
-            eval_gettext "You chose to install oh-my-zsh"; echo
-            git clone https://github.com/ohmyzsh/ohmyzsh.git "${HOME}/.oh-my-zsh"
-            cp "${HOME}/.oh-my-zsh/templates/zshrc.zsh-template" "${HOME}/.zshrc"
-        fi
+            index=1
+            ;;
+        fish)
+            install_one "fish"
 
-        index=1
-        ;;
-    fish)
-        install_one "fish"
+            local current_shell=$(getent passwd "$USER" | cut -d: -f7)
+            while [[ "$current_shell" != "/usr/bin/fish" ]]; do
+                eval_gettext "Setting default shell to fish..."; echo
+                chsh -s "/usr/bin/fish"
+                current_shell=$(getent passwd "$USER" | cut -d: -f7)
+            done
 
-        local current_shell=$(getent passwd $USER | cut -d: -f7)
+            fish -c 'fish_update_completions'
+            fish -c 'set -U fish_greeting'
+            mkdir -p "${HOME}/.config/fish"
+            touch "${HOME}/.config/fish/config.fish"
 
-        while [ "$current_shell" != "/usr/bin/fish" ]; do
-            eval_gettext "Setting default shell to fish..."; echo
-            chsh -s "/usr/bin/fish"
-            current_shell=$(getent passwd $USER | cut -d: -f7)
-        done
-
-        fish -c 'fish_update_completions'
-        fish -c 'set -U fish_greeting'
-        mkdir -p "${HOME}/.config/fish"
-        touch "${HOME}/.config/fish/config.fish"
-        index=2
-        ;;
-    *)
-        eval_gettext "Invalid choice"; echo
-        ;;
+            index=2
+            ;;
+        *)
+            eval_gettext "Invalid choice"; echo
+            return 1
+            ;;
     esac
 
-    # Add aliases to the corresponding shell config file if not already present
-    if [[ $index -ne 3 ]]; then
-        for i in "${alias[@]}"; do
-            if ! grep -q "${i}" "${config_file[$index]}"; then
-                echo "${i}" >>"${config_file[$index]}"
-            fi
-        done
-    fi
+    # Append aliases to the selected shell's config file if not already present
+    for alias_cmd in "${alias_list[@]}"; do
+        if ! grep -q "${alias_cmd}" "${config_file[$index]}"; then
+            echo "${alias_cmd}" >> "${config_file[$index]}"
+        fi
+    done
 }
